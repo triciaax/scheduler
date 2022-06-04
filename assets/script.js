1365c3aeff9b8fe181309bf0a7855732e20617f5
Tricia <tricianghiem@gmail.com>
2022-06-04 13:08:05 -0500

    added custom CSS and styling

--- assets/script.js
+++ assets/script.js
@@ -10,24 +10,47 @@ $("#currentDay").text(now);
 function buildHourBlock(hour) {
   var timeString = buildTimeString(hour);
   var hourBlock = $(
-    `<form data-name="${hour}" class="row justify-content-center"></form>.`
+    `<form data-name="${hour}" class="row justify-content-center ${color(hour)}"></form>.`
   );
   var timeEl = $(
-    `<div class="row justify-content-center">${timeString}</div>`
+    `<div class="col-2 ${color(hour)}">${timeString}</div>`
   );
+
+  //text area for user input is below
   var textEl = $(
-    `<textarea name="${hour}" class="row justify-content-center col-6">${store.getItem(`store-${hour}`) || ""}</textarea>`
+    `<textarea name="${hour}" class="col-9 ${color(hour)}">${store.getItem(`store-${hour}`) || ""}</textarea>`
   );
-  var saveEl = $(`<button class="btn btn-info" type="submit">Save</button>`);
-  hourBlock.append(timeEl, textEl, saveEl);
-  return hourBlock;
+
+  //save button and functionality for user is below
+  var saveEl = $(`<button class="btn btn-dark col-1" type="submit">Save</button>`);
+  hourBlock.submit(function(e) {
+    var hour = $(this).data('name');
+    var text = $(this).find('textarea').val();
+    store.setItem(`store-${hour}`, text);  
+})
+hourBlock.append(timeEl);
+hourBlock.append(textEl);
+hourBlock.append(saveEl);
+return hourBlock;
 }
 
 function buildTimeString(hour) {
-  var timeString = moment(hour, "hA").format("hA");
-  return timeString;
+var timeString = moment(hour, 'hA').format('hA');
+return timeString;
 }
 
-for (var i = 0; i <= 24; i++) {
-    schedulerblock.append(buildHourBlock(i));
+function color(hour) {
+    var currentHour = moment().hour();
+    if (currentHour > hour) {
+        return 'background-past';
+    } else if (currentHour === hour) {
+        return 'background-current';
+    } else {
+        return 'background-future';
+    }
+}
+
+//set the time range from 9AM to 6PM
+for (var i = 9; i <= 18; i++) {
+schedulerblock.append(buildHourBlock(i));
 }
