bbd38d818062aad1e7a429746d81e6a1436dcdca
Tricia <tricianghiem@gmail.com>
2022-06-04 12:41:34 -0500

    updated css

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Work Day Scheduler Starter Code
+## About
+This is a work day scheduler application to help users add important events to a daily planner. In this application, users can add events to specific time blocks and save their changes. Events are color coded to indicate if the event is in the past, future, or present. 
+
+## Color Scheme
+<img valign='middle' alt='grey' src='https://readme-swatches.vercel.app/E0E0E0?style=circle'/> = Past
+<br><img valign='middle' alt='blue' src='https://readme-swatches.vercel.app/9DCCE3?style=circle'/> = Present
+<br><img valign='middle' alt='pink' src='https://readme-swatches.vercel.app/FBAFD6?style=circle'/> = Future
+  
+## How to use this
+1. Land on the work day scheduler. You'll find the current date and see time blocks for each hour
+2. To add an event, click on time block and add your event title and details
+3. To save your changes, click on the "save" button to the right of the time block.
+   
+## Screenshots
+
+## Links
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,33 @@
-//variables defined below 
-var schedulerblock = $('#hour-scheduler');
-var now = moment().format('LL');
+//variables defined below
+var schedulerblock = $("#hour-scheduler");
+var now = moment().format("LL");
 const store = window.localStorage;
 
 //pull dynamically of the current day/date of the week
-$('#currentDay').text(now)
+$("#currentDay").text(now);
 
 //generate blocks for each hour of the day
 function buildHourBlock(hour) {
-    var hourBlock = $('<div>');
-    hourBlock.addClass('styling goes here');
-    hourBlock.attr('id', `hour-${hour}`);
+  var timeString = buildTimeString(hour);
+  var hourBlock = $(
+    `<form data-name="${hour}" class="row justify-content-center"></form>.`
+  );
+  var timeEl = $(
+    `<div class="row justify-content-center">${timeString}</div>`
+  );
+  var textEl = $(
+    `<textarea name="${hour}" class="row justify-content-center col-6">${store.getItem(`store-${hour}`) || ""}</textarea>`
+  );
+  var saveEl = $(`<button class="btn btn-info" type="submit">Save</button>`);
+  hourBlock.append(timeEl, textEl, saveEl);
+  return hourBlock;
+}
+
+function buildTimeString(hour) {
+  var timeString = moment(hour, "hA").format("hA");
+  return timeString;
+}
+
+for (var i = 0; i <= 24; i++) {
+    schedulerblock.append(buildHourBlock(i));
 }
